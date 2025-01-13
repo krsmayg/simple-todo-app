4dc6aa8fe9fb8683a7f4efc97af06cba806fbd93
Minaro M <kiryamayg@gmail.com>
2025-01-13 19:34:17 +0200

    Render simple fake data

--- todo-ui/src/App.tsx
+++ todo-ui/src/App.tsx
@@ -1,13 +1,36 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 import { Todo } from "./types";
-
+import Header from "./ui/Header";
+import ToDoList from "./components/ToDoList";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([])
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchToDos = async () => {
+      try {
+        const res = await fetch("/mock/todos.json");
+        if (!res.ok) {
+          throw new Error(`Could not fetch data ${res.statusText}`);
+        }
+        const resData: Todo[] = await res.json();
+        setTodos(resData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchToDos();
+  }, []);
+
   return (
-    <div>
-      
+    <div className="App">
+      <Header />
+      <input type="text" placeholder="new todo" />
+      <button>Add</button>
+      <ToDoList todos={todos} /> 
     </div>
-  )
-};
-export default App
+  );
+}
+export default App;
