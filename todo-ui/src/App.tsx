061e4f745afb70c1ff758c70fa77b16d9937be25
Minaro M <kiryamayg@gmail.com>
2025-01-13 17:04:44 +0200

    Project initialization

---
+++ todo-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { Todo } from "./types";
+
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  return (
+    <div>
+      
+    </div>
+  )
+};
+export default App
