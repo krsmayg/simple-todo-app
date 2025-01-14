605a164039e1218b09203c13c6a5f222eabdcd8a
Minaro M <kiryamayg@gmail.com>
2025-01-14 02:52:31 +0200

    Refactor Layout

--- todo-ui/src/App.tsx
+++ todo-ui/src/App.tsx
@@ -1,35 +1,15 @@
-import { useEffect, useState } from "react";
-import { Todo } from "./types";
+
 import Header from "./ui/Header";
 import ToDoList from "./components/ToDoList";
+import ToDoForm from "./components/ToDoForm";
+
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchToDos = async () => {
-      try {
-        const res = await fetch("/mock/todos.json");
-        if (!res.ok) {
-          throw new Error(`Could not fetch data ${res.statusText}`);
-        }
-        const resData: Todo[] = await res.json();
-        setTodos(resData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchToDos();
-  }, []);
-
+ 
   return (
     <div className="App">
       <Header />
-      <input type="text" placeholder="new todo" />
-      <button>Add</button>
-      <ToDoList todos={todos} /> 
+      <ToDoForm />
     </div>
   );
 }
