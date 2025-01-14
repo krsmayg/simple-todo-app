3d593175a9ebad428bb86273b3d64b34ef752080
Minaro M <kiryamayg@gmail.com>
2025-01-14 02:48:41 +0200

    Integrate functions with real api

--- todo-ui/src/services/apiTodos.ts
+++ todo-ui/src/services/apiTodos.ts
@@ -1,21 +1,23 @@
 import { Todo } from "@/types";
 
+const api = `https://localhost:7016/api/todos`;
 export async function getTodos(): Promise<Todo[] | undefined> {
   try {
-    const res = await fetch('/mock/todos.json');
+    const res = await fetch(`${api}`);
     if (!res.ok) {
       throw new Error(`Could not fetch data ${res.statusText}`)
     }
-    const resData: Todo[] = await res.json();
-    return resData;
+    const apiData: Todo[] = await res.json();
+    return apiData
+
   } catch (error) {
     console.error(error);
   }
 }
 
-export async function deleteTodo(id: Pick<Todo, 'id'>): Promise<Todo[] | undefined> {
+export async function deleteTodo(id: Todo['id']): Promise<Todo[] | undefined> {
   try {
-    const res = await fetch(`/mock/todos.json?id=${id}`, {
+    const res = await fetch(`${api}?id=${id}`, {
       method: 'DELETE',
     });
     if (!res.ok) {
@@ -27,9 +29,9 @@ export async function deleteTodo(id: Pick<Todo, 'id'>): Promise<Todo[] | undefin
     console.error(error);
   }
 }
-export async function addToDo(newTodo: Omit<Todo, 'id'>): Promise<Todo[] | undefined> {
+export async function createToDo(newTodo: Omit<Todo, 'id'>): Promise<Todo[] | undefined> {
   try {
-    const res = await fetch(`/mock/todos.json`, {
+    const res = await fetch(`${api}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -47,8 +49,22 @@ export async function addToDo(newTodo: Omit<Todo, 'id'>): Promise<Todo[] | undef
 }
 
 
-
-
 export async function updateTodo(todo: Todo) {
-
+  console.log(todo)
+  try {
+    const res = await fetch(`${api}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo)
+    });
+    if (!res.ok) {
+      throw new Error(`Failed to update todo: ${res.statusText}`);
+    }
+    const updatedTodos = await getTodos();
+    return updatedTodos;
+  } catch (error) {
+    console.error(error);
+  }
 }
